97e735cd710b876c33d6449debdf1a57c53f1d98
DenisGo <anywaymaybe1@gmail.com>
2025-01-15 19:24:28 +0400

    feature/include-data-base: изменил конфиги подключения, настроил бд, проверил работоспособность бд на другом компе

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,4 +4,19 @@ import { Sequelize } from 'sequelize-typescript';
 @Injectable()
 export class AppService {
   constructor(private sequelize: Sequelize) {}
+
+  async onModuleInit() {
+    try {
+      await this.sequelize.authenticate(); // Проверяем подключение
+      console.log('Database connection established successfully.');
+    } catch (error) {
+      console.error(
+        `
+        
+        Failed to connect to the database:`,
+        error,
+      );
+      throw new Error('Database connection error');
+    }
+  }
 }
