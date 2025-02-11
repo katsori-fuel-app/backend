f424aa0c4b83e0102a206f3e4141c5a416a3d512
DenisGo <anywaymaybe1@gmail.com>
2025-02-11 18:09:16 +0400

    feature/user-get-one: добавил апи

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Global, Post } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Get,
+    Global,
+    NotFoundException,
+    Param,
+    Post,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UserDto } from './dto';
 
@@ -15,5 +23,16 @@ export class UsersController {
     @Get()
     getAll() {
         return this.usersService.findAll();
+    }
+
+    @Get(':login')
+    async getUser(@Param('login') login: string) {
+        const currentUser = await this.usersService.findOne(login);
+
+        if (!currentUser) {
+            throw new NotFoundException(`Пользователь '${login}' не найден`);
+        }
+
+        return currentUser;
     }
 }
